a0eb2954534d4535a302dbc55a85ab3faeca4925
Tim Smith <tim@cozy.co>
2015-02-24 15:46:03 -0800

    Release 1.1.2

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Tim Smith'
 maintainer_email 'tsmithi84@gmail.com'
 license          'Apache 2.0'
 description      'Installs and configures SmokePing server with fping'
-version          '1.1.1'
+version          '1.1.2'
 
 depends 'apache2'
 depends 'perl'
