6a51e0fbca88c401d467656dd808f351d5ff0c3f
Jason McNew <foonix@yahoo.com>
2016-07-07 17:41:30 -0700

    Add tests to verify vhost function.

--- test/integration/master/serverspec/smokeping_spec.rb
+++ test/integration/master/serverspec/smokeping_spec.rb
@@ -12,3 +12,18 @@ describe 'Smokeping daemon' do
     expect(service('smokeping')).to be_running
   end
 end
+
+describe command('curl -I localhost/smokeping.cgi') do
+  its(:stdout) { should match('HTTP/1.1 200 OK') }
+  its(:exit_status) { should eq(0) }
+end
+
+describe command('curl -I localhost/smokeping/images/smokeping.png') do
+  its(:stdout) { should match('HTTP/1.1 200 OK') }
+  its(:exit_status) { should eq(0) }
+end
+
+describe command('curl http://localhost/smokeping.cgi?target=Production.slave1') do
+  its(:stdout) { should match('Production.slave2') }
+  its(:exit_status) { should eq(0) }
+end
